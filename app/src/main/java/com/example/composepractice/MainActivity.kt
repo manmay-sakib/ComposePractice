7d3964b6fc7e6c43b8d795e3e5a0e9c35f37a6db
manmay sakib <monmy.sakib@gmail.com>
2023-12-22 17:18:22 +0600

    font practice

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -33,10 +33,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -59,9 +65,34 @@ class MainActivity : ComponentActivity() {
                 .background(Color(0xFF101010))
             ){
                 Text(
-                    text = "Jetpack Compose",
+                    text = buildAnnotatedString {
+                                                withStyle(
+                                                    style = SpanStyle(
+                                                        color = Color.Green,
+                                                        fontSize = 50.sp
+                                                    )
+                                                ){
+                                                    append("J")
+                                                }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ){
+                            append("C")
+                        }
+                        append("ompose")
+                    },
                     color = Color.White,
-                    fontSize = 30.sp
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    //textDecoration = TextDecoration.Underline
+
                 )
             }
 
