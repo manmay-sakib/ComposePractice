29e679fe360e8b415ff8e0776960af9212a5cec1
manmay sakib <monmy.sakib@gmail.com>
2023-12-22 16:54:23 +0600

    added font

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -33,6 +34,9 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,87 +45,30 @@ import com.example.composepractice.ui.theme.ComposePracticeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.roboto_thin, FontWeight.Thin),
+            Font(R.font.roboto_light, FontWeight.Light),
+            Font(R.font.roboto_regular, FontWeight.Normal),
+            Font(R.font.roboto_medium, FontWeight.Medium),
+            Font(R.font.roboto_bold, FontWeight.Bold),
+            Font(R.font.roboto_black, FontWeight.Black),
+        )
         setContent {
-            val painter = painterResource(id = R.drawable.dog)
-            val description = "Dog"
-            val title = "Dog is Sweet"
-           Box (
-               modifier = Modifier
-                   .fillMaxWidth(0.5f)
-                   .padding(16.dp)
-           ){
-               ImageCard(
-                   painter = painter,
-                   contentDescription = description,
-                   title = title,
-               )
-           }
-            /*ImageCard(
-                painter = painter,
-                contentDescription = description,
-                title = title,
-            )*/
-
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 5.dp
-        ),
-        modifier = modifier
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp)
-    ) {
-
-        Box(
-            modifier = Modifier.height(200.dp)
-        ) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-                )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
-                        )
-                    )
-            )
-            Box (
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF101010))
             ){
-
                 Text(
-                    title,
-                    style = TextStyle(
-                        color = Color.White,
-                        fontSize = 16.sp
-                    )
-                    )
+                    text = "Jetpack Compose",
+                    color = Color.White,
+                    fontSize = 30.sp
+                )
             }
-        }
 
+            }
+
+        }
     }
 
-}
+
 
