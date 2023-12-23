783a2bc523d2c244802ca6c592a620e42256afb4
manmay sakib <monmy.sakib@gmail.com>
2023-12-23 14:13:08 +0600

    state practice

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -26,6 +27,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -47,59 +50,63 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composepractice.ui.theme.ComposePracticeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val fontFamily = FontFamily(
-            Font(R.font.roboto_thin, FontWeight.Thin),
-            Font(R.font.roboto_light, FontWeight.Light),
-            Font(R.font.roboto_regular, FontWeight.Normal),
-            Font(R.font.roboto_medium, FontWeight.Medium),
-            Font(R.font.roboto_bold, FontWeight.Bold),
-            Font(R.font.roboto_black, FontWeight.Black),
-        )
         setContent {
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFF101010))
-            ){
-                Text(
-                    text = buildAnnotatedString {
-                                                withStyle(
-                                                    style = SpanStyle(
-                                                        color = Color.Green,
-                                                        fontSize = 50.sp
-                                                    )
-                                                ){
-                                                    append("J")
-                                                }
-                        append("etpack ")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp
-                            )
-                        ){
-                            append("C")
-                        }
-                        append("ompose")
-                    },
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic,
-                    textAlign = TextAlign.Center,
-                    //textDecoration = TextDecoration.Underline
+            Column(
+                Modifier.fillMaxSize()
+            ) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ){
+                    color.value = it
+                }
+                Box(modifier = Modifier
+                    // value which is came from the it which is came form
+                    // state will reflects in the background
+                    .background(color.value)
+                    .weight(1f)
+                    .fillMaxSize()
 
                 )
             }
+
 
             }
 
         }
     }
 
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit
+) {
 
+
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+
+                )
+            )
+
+
+        }
+    )
+}
 
